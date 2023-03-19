181d4586fdd13ed9aa03af67ee30049487019a32
alexey126126 <schulgin_alexej@mail.ru>
2023-03-19 17:58:30 +0300

    Initial commit

---
+++ src/main/java/Entity/Human.java
@@ -0,0 +1,16 @@
+package Entity;
+
+import java.util.Date;
+
+public class Human {
+    private int ID;
+    private String name;
+    private String surname;
+    private String patronymic;
+    private Date dateOfBirth;
+    private boolean gender;
+    private int passportSeries;
+    private int passportNumber;
+    private int age;
+
+}
