39bbd72187e13e12fcf813b4b41ebe4816353528
alexey126126 <schulgin_alexej@mail.ru>
2023-03-19 19:48:51 +0300

    init1

--- src/main/java/Entity/Human.java
+++ src/main/java/Entity/Human.java
@@ -11,6 +11,32 @@ public class Human {
     private boolean gender;
     private int passportSeries;
     private int passportNumber;
-    private int age;
+
+    public Human(int ID, String name, String surname, String patronymic, Date dateOfBirth,
+                 boolean gender, int passportSeries, int passportNumber) {
+        this.ID = ID;
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.passportSeries = passportSeries;
+        this.passportNumber = passportNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int createAge(Date date) {
+        date = getDateOfBirth();
+        Date today = new Date();
+        int yearBirth = date.getYear();
+        int yearToday = today.getYear();
+        final int age =  yearToday-yearBirth;
+        return age;
+    }
+
+
 
 }
